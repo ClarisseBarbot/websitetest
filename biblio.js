d490ce053311083ce4af7c6a1dc2e88b8a44d2df
DESIGNTEXTILESPECTACLE <106143106+DESIGNTEXTILESPECTACLE@users.noreply.github.com>
2023-11-03 17:29:53 +0100

    mise en place V1 biblio
    retirer ancien site pour biblio v1

---
+++ biblio.js
@@ -0,0 +1,22 @@
+
+
+//analysis appear on click
+
+
+function r1(){ document.getElementById("cartello").innerHTML = '<h5>Analyse & Abstract</h5> <p> test </p><a class="btn btn-sm btn-outline-dark rounded-pill" href="#">Plantes</a>';}
+   
+function r1out(){ document.getElementById("cartello").innerHTML="";}
+
+function r2(){ document.getElementById("cartello").innerHTML="onMouseOver event ROW 2 applied";}
+function r2out(){document.getElementById("cartello").innerHTML="";}
+
+//print on click
+      
+// Defining the myFunc function
+
+function myFunc() {
+   print = window.print();
+}
+
+
+
